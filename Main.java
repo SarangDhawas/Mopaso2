2d04d136db62c9ad576bc721372d8dc42d21f211
SarangDhawas <113573583+SarangDhawas@users.noreply.github.com>
2022-12-11 19:50:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import org.omg.CORBA.WStringSeqHelper;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.print( a);
+        System.out.print(b);
+    }
+    }
